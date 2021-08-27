983e2c9c4af80ba478bd1528dc20e18f36d772e2
raulportela <raul.p.p@hotmail.com>
2021-08-27 19:01:13 -0300

    correção do link do whats

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <h4>Clique para acessar</h4>
           <a href="https://www.facebook.com/casanovajardimdaspedras"><i className="fab fa-facebook-f"></i></a>
           <a href="https://www.instagram.com/acasanovapizzaria/"><i className="fab fa-instagram"></i></a>
-          <a href="wa.me/+5511996591281"><i className="fab fa-whatsapp"></i></a>
+          <a href="wa.me/5511996591281"><i className="fab fa-whatsapp"></i></a>
           <a href="https://www.google.com/maps/place/Pizzaria+Casa+Nova+Jd.+das+Pedras/@-23.7783968,-46.6839683,15z/data=!4m2!3m1!1s0x0:0x97389a144f77bb3c?sa=X&ved=2ahUKEwjL7Im1ktLyAhXmrJUCHeAzBMIQ_BJ6BQiEARAF"><i className="fas fa-map-marked-alt"></i></a>
         </div>
       </div>
