a7fd4331dffe494d6851d31ba3103e2082d8123d
raulportela <raul.p.p@hotmail.com>
2021-08-27 18:34:13 -0300

    Subir tela de construção

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import '../src/global.scss'
+import '../src/styles/style.scss'
+import logotipo from '../src/img/logotipo.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <main>
+          <img src={logotipo} alt="logotipo" />
+          <h2>Em Breve... <br />Site em construção</h2>
+        </main>
+        <div className="social-icons">
+          <h4>Clique para acessar</h4>
+          <a href="https://www.facebook.com/casanovajardimdaspedras"><i className="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/acasanovapizzaria/"><i className="fab fa-instagram"></i></a>
+          <a href="wa.me/+5511996591281"><i className="fab fa-whatsapp"></i></a>
+          <a href="https://www.google.com/maps/place/Pizzaria+Casa+Nova+Jd.+das+Pedras/@-23.7783968,-46.6839683,15z/data=!4m2!3m1!1s0x0:0x97389a144f77bb3c?sa=X&ved=2ahUKEwjL7Im1ktLyAhXmrJUCHeAzBMIQ_BJ6BQiEARAF"><i className="fas fa-map-marked-alt"></i></a>
+        </div>
+      </div>
     </div>
   );
 }
